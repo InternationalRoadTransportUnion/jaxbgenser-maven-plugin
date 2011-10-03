d537382fe81fd79ae6415feeee0b5a9ea85210bc
Matthieu Verbert <Matthieu.Verbert@iru.org>
2011-10-03 09:28:04 +0200

    Maven plugin to compute serialVersionUID in JAXB serializable
    the serialVersionUID is computed using groupId, artifactId, version
    and packaging

---
+++ src/main/java/org/iru/maven/plugins/jaxbgenser/XJBSerialVersionUIDMojo.java
@@ -0,0 +1,95 @@
+package org.iru.maven.plugins.jaxbgenser;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.security.MessageDigest;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.project.MavenProject;
+import org.dom4j.Document;
+import org.dom4j.DocumentFactory;
+import org.dom4j.Element;
+import org.dom4j.Namespace;
+import org.dom4j.QName;
+import org.dom4j.io.OutputFormat;
+import org.dom4j.io.XMLWriter;
+
+
+/**
+ * 
+ * @phase generate-sources
+ * @goal generate-xjb
+ *
+ */
+public class XJBSerialVersionUIDMojo extends AbstractMojo {
+
+	/**
+	 * @parameter default-value="${project}"
+	 * @required
+	 * @readonly
+	 */
+	private MavenProject project;
+
+	/**
+	 * The directory where to create the bindings files
+	 *
+	 * @parameter default-value="${basedir}/target/xjb"
+	 */
+	private File bindingDirectory;
+
+	/**
+	 * The globalBindings file
+	 * 
+	 * @parameter default-value="globalBindings.xjb"
+	 */
+	private String globalBindings;
+
+	private Namespace JAXB_NAMESPACE = new Namespace("jaxb", "http://java.sun.com/xml/ns/jaxb");
+
+	public void execute() throws MojoExecutionException, MojoFailureException {
+
+		MessageDigest md;
+		try {
+			md = MessageDigest.getInstance("SHA-1");
+			StringBuilder sb = new StringBuilder(project.getGroupId());
+			sb.append(":").append(project.getArtifactId());
+			sb.append(":").append(project.getVersion());
+			sb.append(":").append(project.getPackaging());
+			md.update(sb.toString().getBytes("UTF-8"));
+			byte[] b = md.digest();
+			long l = 0;
+			for (int i = 0; i < 8 ; i++) {
+				if (i != 0)
+					l <<= 8L;	
+				l += (long) b[i];
+			}
+
+			DocumentFactory docf = new DocumentFactory();
+			Document doc = docf.createDocument();
+
+			Element root = docf.createElement(new QName("bindings", JAXB_NAMESPACE));
+			root.addAttribute(new QName("version", JAXB_NAMESPACE), "2.1");
+
+			doc.setRootElement(root);
+			Element gb = docf.createElement(new QName("globalBindings", JAXB_NAMESPACE));
+			root.add(gb);
+			Element serializable = docf.createElement(new QName("serializable", JAXB_NAMESPACE));
+			serializable.addAttribute("uid", Long.toString(l));
+			gb.add(serializable);
+
+
+			OutputFormat format = OutputFormat.createPrettyPrint();
+			format.setIndentSize(4); 
+
+			bindingDirectory.mkdirs();
+			File xjb = new File(bindingDirectory, globalBindings);
+			XMLWriter writer = new XMLWriter(new FileOutputStream(xjb), format);
+			writer.write(doc);
+			writer.close();
+		} catch (Exception e) {
+			throw new MojoExecutionException(e.getMessage(), e);
+		}
+	}
+}
